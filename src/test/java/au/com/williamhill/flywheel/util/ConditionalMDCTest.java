8ae272bb7e18d6996a84ff0cffb37bd6122d4c9e
Emil Koutanov <ekoutanov@gmail.com>
2017-07-02 10:00:40 +1000

    ConditionalMDC test.

---
+++ src/test/java/au/com/williamhill/flywheel/util/ConditionalMDCTest.java
@@ -0,0 +1,41 @@
+package au.com.williamhill.flywheel.util;
+
+import static junit.framework.TestCase.*;
+
+import org.junit.*;
+import org.slf4j.*;
+
+import com.obsidiandynamics.indigo.util.*;
+
+public final class ConditionalMDCTest {
+  @Before
+  public void setup() {
+    MDC.clear();
+  }
+  
+  @After
+  public void teardown() {
+    MDC.clear();
+  }
+  
+  @Test
+  public void testToString() {
+    TestSupport.assertToString(ConditionalMDC.on(() -> true));
+  }
+  
+  @Test
+  public void testConditionPass() {
+    ConditionalMDC.on(() -> true).put("foo", "bar").put("dog", "bark").assign();
+    assertEquals("foo,dog", MDC.get("root"));
+    assertEquals("bar", MDC.get("foo"));
+    assertEquals("bark", MDC.get("dog"));
+  }
+  
+  @Test
+  public void testConditionFail() {
+    ConditionalMDC.on(() -> false).put("foo", "bar").assign();
+    assertNull(MDC.get("root"));
+    assertNull(MDC.get("foo"));
+    assertNull(MDC.get("dog"));
+  }
+}
