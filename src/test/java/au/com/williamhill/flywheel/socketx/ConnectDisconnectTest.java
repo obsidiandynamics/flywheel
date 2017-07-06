295ceb3f500d13e3a0901e2e1871c1f3472e31f5
Emil Koutanov <ekoutanov@gmail.com>
2017-07-06 10:55:17 +1000

    Fixed test timeout.

--- src/test/java/au/com/williamhill/flywheel/socketx/ConnectDisconnectTest.java
+++ src/test/java/au/com/williamhill/flywheel/socketx/ConnectDisconnectTest.java
@@ -73,7 +73,7 @@ public final class ConnectDisconnectTest extends BaseClientServerTest {
     }
 
     // assert connections on server
-    await().dontCatchUncaughtExceptions().atMost(1, SECONDS).untilAsserted(() -> {
+    await().dontCatchUncaughtExceptions().atMost(60, SECONDS).untilAsserted(() -> {
       Mockito.verify(serverListener, Mockito.times(connections)).onConnect(Mocks.anyNotNull());
       Mockito.verify(clientListener, Mockito.times(connections)).onConnect(Mocks.anyNotNull());
     });
