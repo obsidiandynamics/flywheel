3ff0f10b6364fa84cbbf325ebfd842ebdec6f110
Emil Koutanov <ekoutanov@gmail.com>
2017-07-06 17:10:52 +1000

    Started working on KafkaReceiver

---
+++ backplane-kafka/src/main/java/au/com/williamhill/flywheel/backplane/kafka/KafkaReceiver.java
@@ -0,0 +1,47 @@
+package au.com.williamhill.flywheel.backplane.kafka;
+
+import org.apache.kafka.clients.consumer.*;
+
+public class KafkaReceiver<K, V> extends Thread implements AutoCloseable {
+  @FunctionalInterface
+  public interface RecordHandler<K, V> {
+    void handle(ConsumerRecords<K, V> records);
+  }
+  
+  private final Consumer<K, V> consumer;
+  
+  private final long pollTimeoutMillis;
+  
+  private final RecordHandler<K, V> handler;
+  
+  private volatile boolean running = true;
+  
+  public KafkaReceiver(Consumer<K, V> consumer, long pollTimeoutMillis, String name, RecordHandler<K, V> handler) {
+    super(name);
+    this.consumer = consumer;
+    this.pollTimeoutMillis = pollTimeoutMillis;
+    this.handler = handler;
+    start();
+  }
+  
+  @Override 
+  public void run() {
+    while (running) {
+      final ConsumerRecords<K, V> records = consumer.poll(pollTimeoutMillis);
+      if (! records.isEmpty()) {
+        handler.handle(records);
+      }
+    }
+    consumer.close();
+  }
+  
+  @Override
+  public void close() throws InterruptedException {
+    running = false;
+    interrupt();
+  }
+  
+  public void await() throws InterruptedException {
+    join();
+  }
+}
