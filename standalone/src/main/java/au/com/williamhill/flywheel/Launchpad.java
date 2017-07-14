602bc5802c0bd7ead432465f77cd15a834f4ec0f
Emil Koutanov <ekoutanov@gmail.com>
2017-07-14 15:22:41 +1000

    More Launchpad work; additional Launchpad coverage

--- standalone/src/main/java/au/com/williamhill/flywheel/Launchpad.java
+++ standalone/src/main/java/au/com/williamhill/flywheel/Launchpad.java
@@ -10,44 +10,59 @@ import org.slf4j.*;
 import au.com.williamhill.flywheel.yconf.*;
 
 public final class Launchpad {
-  static final class LauncherException extends Exception {
+  private static final String DEF_PROFILE = "conf/default";
+  
+  static final class LaunchpadException extends Exception {
     private static final long serialVersionUID = 1L;
     
-    LauncherException(String m, Throwable cause) { super(m, cause); }
+    LaunchpadException(String m, Throwable cause) { super(m, cause); }
   }
   
-  Launchpad(File profilePath) throws LauncherException {
+  private final Profile profile;
+  
+  Launchpad(File profilePath) throws LaunchpadException {
     if (! profilePath.exists()) {
-      throw new LauncherException("Profile path " + profilePath + " does not exist", null);
+      throw new LaunchpadException("Profile path " + profilePath + " does not exist", null);
     }
     
     if (! profilePath.isDirectory()) {
-      throw new LauncherException("Profile path must be a directory", null);
+      throw new LaunchpadException("Profile path must be a directory", null);
     }
     
     final File profileYaml = new File(profilePath.getPath() + "/profile.yaml");
     if (! profileYaml.exists() || ! profileYaml.isFile()) {
-      throw new LauncherException("Profile configuration " + profileYaml + " is missing", null);
+      throw new LaunchpadException("Profile configuration " + profileYaml + " is missing", null);
     }
     
-    final Profile p;
     try {
-      p = Profile.fromFile(profileYaml);
+      profile = Profile.fromFile(profileYaml);
     } catch (Exception e) {
-      throw new LauncherException("Error reading profile", e);
+      throw new LaunchpadException("Error reading profile", e);
     }
     
     final StringBuilder sb = new StringBuilder("\nproperties:");
-    for (Map.Entry<String, ?> entry : p.properties.entrySet()) {
+    for (Map.Entry<String, ?> entry : profile.properties.entrySet()) {
       sb.append("\n  ").append(entry.getKey()).append(": ").append(entry.getValue());
       final String unmasked = Masked.unmask(entry.getValue());
       System.setProperty(entry.getKey(), unmasked);
     }
     
-    sb.append("\nlauncher: ").append(p.launcher);
+    sb.append("\nlauncher: ").append(profile.launcher);
     
     final Logger log = LoggerFactory.getLogger(Launchpad.class);
     log.info(sb.toString());
+  }
+  
+  public void launch(String[] args) throws LaunchpadException {
+    try {
+      profile.launcher.launch(args, profile);
+    } catch (Exception e) {
+      throw new LaunchpadException("Failed to launch " + profile.launcher, e);
+    }
+  }
+  
+  public Profile getProfile() {
+    return profile;
   }
   
   public static void main(String[] args) {
@@ -63,18 +78,14 @@ public final class Launchpad {
       if (profileEnv != null) {
         profilePath = new File(profileEnv);
       } else {
-        err.format("Error: no profile name specified.\n");
-        err.format("Point either the %s system property or the %s environment variable to the profile directory.\n",
-                   propertyName, envName);
-        System.exit(1);
-        return;
+        profilePath = new File(DEF_PROFILE);
       }
     }
     
     try {
-      new Launchpad(profilePath);
-    } catch (LauncherException e) {
-      err.format("Error: " + e);
+      new Launchpad(profilePath).launch(args);
+    } catch (LaunchpadException e) {
+      err.format("Error:\n");
       e.printStackTrace(err);
       System.exit(1);;
     }
--- standalone/src/test/java/au/com/williamhill/flywheel/LaunchpadTest.java
+++ standalone/src/test/java/au/com/williamhill/flywheel/LaunchpadTest.java
@@ -1,5 +1,7 @@
 package au.com.williamhill.flywheel;
 
+import static junit.framework.TestCase.*;
+
 import java.io.*;
 
 import org.junit.*;
@@ -7,23 +9,44 @@ import org.junit.*;
 import au.com.williamhill.flywheel.Launchpad.*;
 
 public final class LaunchpadTest {
-  @Test(expected=LauncherException.class)
-  public void testPathDoesNotExit() throws LauncherException {
-    new Launchpad(new File("foo/bar"));
+  @Before
+  public void before() {
+    clearProps();
+  }
+  
+  @After
+  public void after() {
+    clearProps();
+  }
+  
+  private static void clearProps() {
+    System.clearProperty("TestLauncher.a");
+    System.clearProperty("TestLauncher.b");
+  }
+  
+  @Test(expected=LaunchpadException.class)
+  public void testPathDoesNotExit() throws LaunchpadException {
+    new Launchpad(new File("foo/bar")).launch(new String[0]);
   }
 
-  @Test(expected=LauncherException.class)
-  public void testPathNotADirectory() throws LauncherException {
-    new Launchpad(new File("conf/test/profile.yaml"));
+  @Test(expected=LaunchpadException.class)
+  public void testPathNotADirectory() throws LaunchpadException {
+    new Launchpad(new File("conf/test/profile.yaml")).launch(new String[0]);
   }
 
-  @Test(expected=LauncherException.class)
-  public void testProfileMissing() throws LauncherException {
-    new Launchpad(new File("conf"));
+  @Test(expected=LaunchpadException.class)
+  public void testProfileMissing() throws LaunchpadException {
+    new Launchpad(new File("conf")).launch(new String[0]);
   }
 
   @Test
-  public void testDefault() throws LauncherException {
-    new Launchpad(new File("conf/test"));
+  public void testDefault() throws LaunchpadException {
+    final Launchpad launchpad = new Launchpad(new File("conf/test"));
+    launchpad.launch(new String[0]);
+    assertNotNull(launchpad.getProfile().launcher);
+    assertEquals(TestLauncher.class, launchpad.getProfile().launcher.getClass());
+    assertTrue(((TestLauncher) launchpad.getProfile().launcher).launched);
+    assertEquals("a", System.getProperty("TestLauncher.a"));
+    assertEquals("b", System.getProperty("TestLauncher.b"));
   }
 }
