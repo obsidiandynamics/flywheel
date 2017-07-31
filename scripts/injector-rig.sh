4d9de3695fe2b8312c1abe4b6eaaaf3b8b56bc06
ekoutanov <ekoutanov@gmail.com>
2017-07-31 22:46:00 +1000

    Updated rig scripts

--- scripts/injector-rig.sh
+++ scripts/injector-rig.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 JVM_ARGS="-server -XX:-MaxFDLimit -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking -Xms1G -Xmx2G -Xss1M -XX:+UseParallelGC"
+./gradlew -x test testJar
 java $JVM_ARGS -cp build/libs/flywheel-core-test-*.jar $@ au.com.williamhill.flywheel.rig.InjectorRigBenchmark
