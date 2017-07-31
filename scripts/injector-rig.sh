6fed30470f987abf9b1ac887358f9578e87dcfa9
Emil Koutanov <ekoutanov@gmail.com>
2017-07-31 17:19:59 +1000

    Scripts for rig benchmarks.

---
+++ scripts/injector-rig.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+JVM_ARGS="-server -XX:-MaxFDLimit -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking -Xms1G -Xmx2G -Xss1M -XX:+UseParallelGC"
+java $JVM_ARGS -cp build/libs/flywheel-core-test-*.jar $@ au.com.williamhill.flywheel.rig.InjectorRigBenchmark
