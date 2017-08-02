118a66d9b836d4ab70ca1cd09dc2d37aeb46d42e
ekoutanov <ekoutanov@gmail.com>
2017-08-03 07:56:00 +1000

    Capping direct memory in benchmark scripts

--- scripts/remote-rig.sh
+++ scripts/remote-rig.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-JVM_ARGS="-server -XX:-MaxFDLimit -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking -Xms1G -Xmx2G -Xss1M -XX:+UseParallelGC"
+JVM_ARGS="-server -XX:-MaxFDLimit -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking -Xms1G -Xmx4G -Xss1M -XX:MaxDirectMemorySize=2G -XX:+UseParallelGC"
 ./gradlew -x test testJar
 java $JVM_ARGS -cp build/libs/flywheel-core-test-*.jar $@ au.com.williamhill.flywheel.rig.RemoteRigBenchmark
