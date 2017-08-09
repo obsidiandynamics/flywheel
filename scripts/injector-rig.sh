163d271a38ca333300f3770675156b54fdb53296
Emil Koutanov <ekoutanov@gmail.com>
2017-08-09 06:53:44 +0000

    Updated benchmarking params and added GC logging

--- scripts/injector-rig.sh
+++ scripts/injector-rig.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-JVM_ARGS="-server -XX:-MaxFDLimit -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking -Xms1G -Xmx2G -Xss1M -XX:MaxDirectMemorySize=2G -XX:+UseParallelGC"
+JVM_ARGS="-XX:-MaxFDLimit -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking \
+          -Xms1G -Xmx2G -Xss1M -XX:MaxDirectMemorySize=3G \
+          -XX:+DisableExplicitGC -Xloggc:log/injector-gc-%t.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps \
+          -XX:+UseParallelGC"
 ./gradlew -x test testJar
+mkdir -p log
 java $JVM_ARGS -cp build/libs/flywheel-core-test-*.jar $@ au.com.williamhill.flywheel.rig.InjectorRigBenchmark
