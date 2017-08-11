6a9c556d12f88a100ff45942796c8ae8fcdf8fb0
Emil Koutanov <ekoutanov@gmail.com>
2017-08-12 08:52:59 +1000

    Added the `-XX:+ExplicitGCInvokesConcurrent` option.

--- scripts/injector-rig.sh
+++ scripts/injector-rig.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 JVM_ARGS="-XX:-MaxFDLimit -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseCondCardMark -XX:-UseBiasedLocking \
           -Xms1G -Xmx2G -Xss1M -XX:MaxDirectMemorySize=3G \
-          -XX:+DisableExplicitGC -Xloggc:log/injector-gc-%t.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps \
+          -XX:+ExplicitGCInvokesConcurrent -Xloggc:log/injector-gc-%t.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps \
           -XX:+UseParallelGC"
 ./gradlew -x test testJar
 mkdir -p log
