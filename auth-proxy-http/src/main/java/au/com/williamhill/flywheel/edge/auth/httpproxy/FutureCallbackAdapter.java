ad002247d46a6bd98a86d64d705776c64753d557
Emil Koutanov <ekoutanov@gmail.com>
2017-08-19 10:30:15 +1000

    Added FutureCallbackAdapter

---
+++ auth-proxy-http/src/main/java/au/com/williamhill/flywheel/edge/auth/httpproxy/FutureCallbackAdapter.java
@@ -0,0 +1,14 @@
+package au.com.williamhill.flywheel.edge.auth.httpproxy;
+
+import org.apache.http.concurrent.*;
+
+public interface FutureCallbackAdapter<T> extends FutureCallback<T> {
+  @Override
+  public default void completed(T result) {}
+
+  @Override
+  public default void failed(Exception ex) {}
+
+  @Override
+  public default void cancelled() {}
+}
