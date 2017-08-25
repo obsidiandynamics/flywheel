679c5980c1a41bd3175e6e4a3b253f0e14fa3ec4
Emil Koutanov <ekoutanov@gmail.com>
2017-08-25 16:58:11 +1000

    Increased test coverage

---
+++ src/test/java/au/com/williamhill/flywheel/edge/auth/PubSubAuthConfigTest.java
@@ -0,0 +1,44 @@
+package au.com.williamhill.flywheel.edge.auth;
+
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.util.*;
+
+import org.junit.*;
+
+import com.obsidiandynamics.yconf.*;
+
+import au.com.williamhill.flywheel.edge.auth.AuthChain.*;
+
+public final class PubSubAuthConfigTest {
+  @Y
+  public static final class AuthChainSet {
+    @YInject
+    PubAuthChain pub;
+    
+    @YInject
+    SubAuthChain sub;
+  }
+  
+  @Test
+  public void test() throws IOException {
+    final AuthChainSet chains = new MappingContext()
+        .withParser(new SnakeyamlParser())
+        .fromStream(PubSubAuthConfigTest.class.getClassLoader().getResourceAsStream("auth-chain-config.yaml"))
+        .map(AuthChainSet.class);
+    
+    doAssert(chains.pub);
+    doAssert(chains.sub);
+  }
+  
+  private static void doAssert(AuthChain<?> chain) {
+    assertNotNull(chain);
+    assertNotNull(chain.toString());
+    final CombinedMatches matches = chain.getMatches(Collections.singleton("topic"));
+    assertNotNull(matches);
+    assertEquals(1, matches.numAuthenticators);
+    assertEquals(1, matches.matches.size());
+    assertNotNull(matches.toString());
+  }
+}
