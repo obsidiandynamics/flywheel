5826e1872a9f73dfc685b781e83e0e1e5c38a6a9
Emil Koutanov <ekoutanov@gmail.com>
2017-09-08 13:38:02 +1000

    Enforcing minimum ulimit.

---
+++ src/test/java/au/com/williamhill/flywheel/UlimitTest.java
@@ -0,0 +1,21 @@
+package au.com.williamhill.flywheel;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import org.slf4j.*;
+
+import com.obsidiandynamics.indigo.util.*;
+
+public class UlimitTest {
+  @Test
+  public void test() {
+    final int minLimit = 1024;
+    final StringBuilder sb = new StringBuilder();
+    BashInteractor.execute("ulimit -Sn", true, sb::append);
+    final int limit = Integer.parseInt(sb.toString().trim());
+    final Logger logger = LoggerFactory.getLogger(UlimitTest.class);
+    logger.debug("File limit is {}", limit);
+    assertTrue("Limit is too low " + limit, limit >= minLimit);
+  }
+}
