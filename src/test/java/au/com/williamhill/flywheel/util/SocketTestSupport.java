5227ce12f24d53739e27715c3f9b0428a79efb85
Emil Koutanov <ekoutanov@gmail.com>
2017-09-25 06:19:51 +1000

    Added TODOs for subsequent removal of these classes.

--- src/test/java/au/com/williamhill/flywheel/util/SocketTestSupport.java
+++ src/test/java/au/com/williamhill/flywheel/util/SocketTestSupport.java
@@ -11,6 +11,7 @@ import com.obsidiandynamics.indigo.util.*;
 
 import au.com.williamhill.flywheel.socketx.util.*;
 
+//TODO replace with socketx
 public interface SocketTestSupport {
   static int MIN_PORT = 1024;
   static int MAX_PORT = 49151;
